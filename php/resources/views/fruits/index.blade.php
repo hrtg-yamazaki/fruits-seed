b883f76c9488c0b165ad7f355d86983e22517827
hrtg-yamazaki <hrtg.yamazaki@gmail.com>
2021-02-25 23:58:19 +0900

    レイアウトの調整

--- php/resources/views/fruits/index.blade.php
+++ php/resources/views/fruits/index.blade.php
@@ -5,13 +5,29 @@
 
 
 <div class="mx-auto w-75 py-5">
-    <ul>
-        @foreach($fruits as $fruit)
-            <li>
-                {{ $fruit->name }} : ￥{{ $fruit->price }}
-            </li>
-        @endforeach
-    </ul>
+    <div class="card">
+        <div class="card-body">
+            <h3 class="text-dark text-center mb-3">
+                Fruits list
+            </h3>
+            <table class="table table-striped text-center" border="1">
+                <thead class="thead-dark">
+                    <tr>
+                        <th>name</th>
+                        <th>price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($fruits as $fruit)
+                        <tr>
+                            <td>{{ $fruit->name }}</td>
+                            <td>{{ $fruit->price }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
 
 
