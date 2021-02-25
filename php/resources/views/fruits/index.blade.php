62d1ac6319e19a6cb4a2c2097aec052bb6f11ff2
hrtg-yamazaki <hrtg.yamazaki@gmail.com>
2021-02-25 22:23:59 +0900

    レイアウトファイルの準備

--- php/resources/views/fruits/index.blade.php
+++ php/resources/views/fruits/index.blade.php
@@ -1,7 +1,18 @@
-<ul>
-    @foreach($fruits as $fruit)
-        <li>
-            {{ $fruit->name }} : ￥{{ $fruit->price }}
-        </li>
-    @endforeach
-</ul>
+@extends('layout')
+
+
+@section('content')
+
+
+<div class="mx-auto w-75 py-5">
+    <ul>
+        @foreach($fruits as $fruit)
+            <li>
+                {{ $fruit->name }} : ￥{{ $fruit->price }}
+            </li>
+        @endforeach
+    </ul>
+</div>
+
+
+@endsection
---
+++ php/resources/views/layout.blade.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>sample of reintro</title>
+        <link rel="stylesheet"
+                href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+                integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+                crossorigin="anonymous">
+    </head>
+    <body>
+
+        <div id="app" class="container">
+            @yield('content')
+        </div>
+        
+    </body>
+</html>
