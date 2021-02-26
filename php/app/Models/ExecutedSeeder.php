0e38a43cee983cfd65d60539b2d3ca4cfea15e56
hrtg-yamazaki <hrtg.yamazaki@gmail.com>
2021-02-26 22:40:42 +0900

    ExecutedSeederモデルの作成

---
+++ php/app/Models/ExecutedSeeder.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ExecutedSeeder extends Model
+{
+    /**
+     * DBテーブル名
+     * @var string
+     */
+    protected $table = 'executed_seeders';
+}
