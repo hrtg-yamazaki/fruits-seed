d0dea4455daaa6a9383fd02b96f4d3e413a53ed4
Hirotsugu Yamazaki <56186791+hrtg-yamazaki@users.noreply.github.com>
2021-02-28 23:07:36 +0900

    Revert "seederたちに重複実行を回避させつつ、insertにも対応させてみたい"

--- php/database/seeds/DatabaseSeeder.php
+++ php/database/seeds/DatabaseSeeder.php
@@ -1,17 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use App\Models\ExecutedSeeder;
-use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
-    /** 実行したいSeederをここに登録 */
-    private const SEEDERS = [
-        SourFruitsSeeder::class,
-        SweetFruitsSeeder::class,
-    ];
-
     /**
      * Seed the application's database.
      *
@@ -19,52 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        echo "\n";
-        foreach (self::SEEDERS as $seeder) {
-            $this->runOneOnce($seeder);
-        }
-        echo "\n";
+        // $this->call(UsersTableSeeder::class);
     }
-
-    /**
-     * 個別実行 (初めて実行する Seeder のみを実行する)
-     * @param  string  $seederName
-     * @return bool
-     */
-    private function runOneOnce(string $seederName): bool
-    {
-        if (self::isDuplicated($seederName)) {
-            echo "Already executed: ".$seederName."\n";
-            return false;
-        }
-
-        DB::transaction(function () use ($seederName) {
-            $this->call($seederName);
-            self::createExecutedSeeder($seederName);
-        });
-        return true;
-    }
-
-    /**
-     * 名前が重複しているレコードがあるかどうか
-     * @param string $seederName
-     * @return bool
-     */
-    private static function isDuplicated($seederName): bool
-    {
-        $sameNamed = ExecutedSeeder::where('name', $seederName)->first();
-        return isset($sameNamed) ? true : false;
-    }
-
-    /**
-     * Seeder の利用実績を管理するテーブルに、クラス名を保存する
-     * @param string $seederName
-     * @return bool
-     */
-    private static function createExecutedSeeder(string $seederName): bool
-    {
-        $executed = new ExecutedSeeder(["name" => $seederName]);
-        return $executed->save();
-    }
-
 }
