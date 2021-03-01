ae3aa3e92197fbbcacf157c12bef82cc0370c0fa
hrtg-yamazaki <hrtg.yamazaki@gmail.com>
2021-03-01 22:26:32 +0900

    fruitsの有無によるindexページの条件分岐

--- php/resources/views/fruits/index.blade.php
+++ php/resources/views/fruits/index.blade.php
@@ -10,22 +10,29 @@
             <h3 class="text-dark text-center mb-3">
                 Fruits list
             </h3>
-            <table class="table table-striped text-center" border="1">
-                <thead class="thead-dark">
-                    <tr>
-                        <th>name</th>
-                        <th>price</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($fruits as $fruit)
+            @if($fruits->toArray())
+                <table class="table table-striped text-center" border="1">
+                    <thead class="thead-dark">
                         <tr>
-                            <td>{{ $fruit->name }}</td>
-                            <td>{{ $fruit->price }}</td>
+                            <th>name</th>
+                            <th>price</th>
                         </tr>
-                    @endforeach
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        
+                            @foreach($fruits as $fruit)
+                                <tr>
+                                    <td>{{ $fruit->name }}</td>
+                                    <td>{{ $fruit->price }}</td>
+                                </tr>
+                            @endforeach
+                        
+                    </tbody>
+                </table>
+            @else
+                <p>No fruits here.</p>
+                <p>Please exec: "make seed".</p>
+            @endif
         </div>
     </div>
 </div>
